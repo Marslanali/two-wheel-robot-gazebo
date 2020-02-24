6f687b8c78047abfa37c0451bc9b12d586239eb2
marslanali <arslanali800@hotmail.com>
2020-02-24 15:46:37 +0500

    creating new package in current repository

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/arslanali/swarm-gazebo-simulations/src
+# Build directory: /home/arslanali/swarm-gazebo-simulations/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(swarm_robot_aggregation_simulation)
+subdirs(swarm_robot_description)
+subdirs(swarm_robot_gazebo)
+subdirs(swarm_robot_teleop_keyboard)
---
+++ build/swarm_robot_teleop_keyboard/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arslanali/swarm-gazebo-simulations/src/swarm_robot_teleop_keyboard
+# Build directory: /home/arslanali/swarm-gazebo-simulations/build/swarm_robot_teleop_keyboard
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
